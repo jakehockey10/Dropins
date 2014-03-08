cf2a65b6df8dc92162638fbe5517fa5acbcfaff8
Jake Smith <jakehockey10@gmail.com>
2014-03-08 15:00:46 -0700

    working carousel

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.2'
 gem 'bootstrap-sass', '~> 3.1.1'
+gem 'bootstrap_form'
 gem 'bcrypt-ruby', '3.1.2'
 gem 'faker', '1.1.2'
 gem 'will_paginate', '3.0.4'
--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,23 +1,26 @@
 $(document).ready(function () {
-    $('#myCarousel').carousel({
-        interval: 4000
+    $(".jcarousel").jcarousel({
+        animation: 'slow'
     });
-
-    $('.carousel .item').each(function(){
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        for (var i = 0; i < 2; i++) {
-            if (next.next().length > 0) {
-                next.next().children(':first-child').clone().appendTo($(this));
-            }
-            else {
-                $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-            }
-            next = next.next();
-        }
-    });
+//    $('#myCarousel').carousel({
+//        interval: 4000
+//    });
+//
+//    $('.carousel .item').each(function(){
+//        var next = $(this).next();
+//        if (!next.length) {
+//            next = $(this).siblings(':first');
+//        }
+//        next.children(':first-child').clone().appendTo($(this));
+//
+//        for (var i = 0; i < 2; i++) {
+//            if (next.next().length > 0) {
+//                next.next().children(':first-child').clone().appendTo($(this));
+//            }
+//            else {
+//                $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+//            }
+//            next = next.next();
+//        }
+//    });
 });
