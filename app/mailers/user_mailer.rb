ec81b2207d2340912d80cad44d81badda785c9ba
Jake Smith <jakehockey10@gmail.com>
2014-03-10 01:22:24 -0600

    fixing mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: 'jakehockey10@gmail.com'
+  default from: 'therosenbridge@gmail.com'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
