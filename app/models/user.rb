af9e867529e8f34183eb5bb5f5822dcd1527b5e2
Jake Smith <jakehockey10@gmail.com>
2014-06-29 15:42:04 -0600

    adding back button visibility

--- app/models/user.rb
+++ app/models/user.rb
@@ -96,9 +96,9 @@ class User < ActiveRecord::Base
   #   commitments.find_by(dropin_id: dropin.id).destroy
   # end
 
-  # def show_pay_for_dropin_button(dropin)
-  #   self.has_wepay_account? && dropin.user.has_wepay_account? && self.has_not_paid(dropin)
-  # end
+  def show_pay_for_dropin_button(dropin)
+    self.has_wepay_account? && dropin.user.has_wepay_account? && self.has_not_paid(dropin)
+  end
 
   def has_not_paid(dropin)
     Commitment.where(user_id: self.id, dropin_id: dropin.id).blank?
