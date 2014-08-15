bfe7130952244690c7ff06fa1e77fd986a355bb0
Jake Smith <jakehockey10@gmail.com>
2014-08-15 17:20:21 -0600

    fix controller method

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -25,7 +25,7 @@ class InviteController < ApplicationController
   def oauth2callback
     @contacts = request.env['omnicontacts.contacts']
     @user = request.env['omnicontacts.user']
-    puts "List of contacts of #{user[:name]} obtained from #{params[:importer]}:"
+    puts "List of contacts of #{current_user.name} obtained from #{params[:importer]}:"
     @contacts.each do |contact|
       puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
     end
