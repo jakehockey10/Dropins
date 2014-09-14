d9ecf8d9e9e5901e64aac2797ed6b38ae8249e2c
Jake Smith <jakehockey10@gmail.com>
2014-09-13 19:09:09 -0600

    fixing edit dropin

--- app/controllers/dropins_controller.rb
+++ app/controllers/dropins_controller.rb
@@ -18,7 +18,6 @@ class DropinsController < ApplicationController
   end
 
   def edit
-    # TODO: Do a flash message if error occurs.
   end
 
   def create
@@ -39,7 +38,9 @@ class DropinsController < ApplicationController
   end
 
   def update
-    if @dropin.update(dropin_params)
+    the_dropin_params = dropin_params
+    the_dropin_params[:date] = Time.strptime(the_dropin_params[:date], '%m/%d/%Y %I:%M %p')
+    if @dropin.update(the_dropin_params)
       flash[:success] = 'Dropin was successfully updated.'
       redirect_to @dropin
     else
