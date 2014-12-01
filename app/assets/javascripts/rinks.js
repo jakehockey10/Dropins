5a08a278686623689a9eebe1739295df98a968be
Jake Smith <jakehockey10@gmail.com>
2014-12-01 00:44:21 -0700

    styling

--- app/assets/javascripts/rinks.js
+++ app/assets/javascripts/rinks.js
@@ -1,14 +1,14 @@
 //# Place all the behaviors and hooks related to the matching controller here.
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).on('ready page:load', function (){
-  $('.view-rink').tooltip({
-      container: 'body'
-  });
-  $('.edit-rink').tooltip({
-      container: 'body'
-  });
-  $('.delete-rink').tooltip({
-      container: 'body'
-  });
-});
+//$(document).on('ready page:load', function (){
+//  $('.view-rink').tooltip({
+//      container: 'body'
+//  });
+//  $('.edit-rink').tooltip({
+//      container: 'body'
+//  });
+//  $('.delete-rink').tooltip({
+//      container: 'body'
+//  });
+//});
