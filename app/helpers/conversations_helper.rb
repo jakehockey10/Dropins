1302d371e7eeb5931999987d1ff01ada85e56758
Jake Smith <jakehockey10@gmail.com>
2014-12-03 20:16:35 -0700

    fix up conversation starter with tokenized input

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,11 @@
 module ConversationsHelper
+
+  def tokenfield_source
+    source = []
+    current_user.followed_users.each do |u|
+      source << { value: u.email, label: u.name, profile_picture: raw(avatar_for u, {size: :small, class: 'media-object'}) }
+    end
+    raw(source.to_json)
+  end
+
 end
