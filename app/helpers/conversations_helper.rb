600aa1a594271ea16e0bbaa0e76b7e795b7332c0
Jake Smith <jakehockey10@gmail.com>
2015-02-17 23:19:37 -0700

    file formatting

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -3,7 +3,7 @@ module ConversationsHelper
   def tokenfield_source
     source = []
     current_user.followed_users.each do |u|
-      source << { value: u.email, label: u.name, profile_picture: raw(avatar_for u, {size: :small, class: 'media-object'}) }
+      source << { value: u.email, label: u.name, profile_picture: raw(avatar_for u, { size: :small, class: 'media-object' }) }
     end
     raw(source.to_json)
   end
