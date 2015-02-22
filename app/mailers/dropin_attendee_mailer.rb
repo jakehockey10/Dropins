0b7f212e19252681e5a4d71a3fc3c6c94295dbbc
Jake Smith <jakehockey10@gmail.com>
2015-02-21 17:29:09 -0700

    more commits that were untracked

---
+++ app/mailers/dropin_attendee_mailer.rb
@@ -0,0 +1,14 @@
+class DropinAttendeeMailer < ActionMailer::Base
+  default from: ENV['GMAIL_USERNAME']
+
+  def email_attendees(user_id, users, dropin_id, subject, message)
+    @owner = User.find(user_id)
+    @users = users
+    @dropin = Dropin.find(dropin_id)
+    @url = dropin_url(dropin_id)
+    @signup_url = signup_url
+    @subject = subject
+    @message = message
+    mail to: users, subject: subject, from: @owner.email
+  end
+end
