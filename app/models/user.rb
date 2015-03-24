955b5bf8a12a4caff434fed3e4461496dca5a391
Jake Smith <jakehockey10@gmail.com>
2015-03-24 17:36:03 -0600

    fixed state_machine (use someone's fork)

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,8 +59,18 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, content_type: /\Aimage/
   validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
 
-  def active?
-    STATES.key(self.state) === :active
+  state_machine :state, initial: :inactive do
+    STATES.each do |name, value|
+      state name, value: value
+    end
+
+    event :activate do
+      transition all => :active
+    end
+
+    event :deactivate do
+      transition all => :inactive
+    end
   end
 
   def User.new_token
