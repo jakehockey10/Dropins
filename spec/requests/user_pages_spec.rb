af88b8bca575093a8da18e8a17b9ebea4273a78b
Jake Smith <jakehockey10@gmail.com>
2015-05-11 21:18:04 -0600

    fix one test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -179,7 +179,7 @@ describe 'User pages' do
     end
 
     describe 'page' do
-      it { should have_content('Update your profile') }
+      it { should have_content('Manage your profile') }
       it { should have_title('Edit user') }
       it { should have_link('change', href: 'http://gravatar.com/emails') }
     end
